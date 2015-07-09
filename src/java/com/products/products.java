81ab177cd06ed5cf8b404e78b405eb5bd0374dc9
c0654504 <c0654504@SB-N208-07.lambton.on.ca>
2015-07-09 19:43:31 -0400

    Creating products class with different methods such as getConnection,doPut,doDelete,doPost,getResults in it

--- src/java/com/products/products.java
+++ src/java/com/products/products.java
@@ -6,14 +6,34 @@
 
 package com.products;
 
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.stream.JsonParser;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import org.json.simple.JSONArray;
 
 /**
  * REST Web Service
@@ -23,13 +43,27 @@ import javax.ws.rs.PUT;
 @Path("products")
 public class products {
 
-    @Context
-    private UriInfo context;
+    private static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.err.println("JDBC Driver Not Found: " + ex.getMessage());
+        }
 
-    /**
-     * Creates a new instance of products
-     */
-    public products() {
+        try {
+            String jdbc = "jdbc:mysql://ipro.lambton.on.ca/inventory";
+            conn = DriverManager.getConnection(jdbc, "products", "products");
+        } catch (SQLException ex) {
+            System.err.println("Failed to Connect: " + ex.getMessage());
+        }
+        return conn;
+    }
+    
+    @GET
+    @Produces("application/json")
+    public String doGet() {
+        return getResults("SELECT * FROM product");
     }
 
     /**
@@ -37,19 +71,111 @@ public class products {
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces("application/xml")
-    public String getXml() {
-        //TODO return proper representation object
-        throw new UnsupportedOperationException();
+    @Produces("application/json")
+    @Path("{id}")
+    public String doGet(@PathParam("id") String productID) {
+        return getResults("SELECT * FROM product WHERE productID = ?", productID);
     }
 
-    /**
-     * PUT method for updating or creating an instance of products
-     * @param content representation for the resource
-     * @return an HTTP response with content of the updated or created resource.
-     */
-    @PUT
-    @Consumes("application/xml")
-    public void putXml(String content) {
+    @POST
+    @Consumes("application/json")
+    public void doPost(String str) {
+        JsonParser parser = Json.createParser(new StringReader(str));
+        Map<String, String> mapKeyValue = new HashMap<>();
+        String key = "", val;
+        while (parser.hasNext()) {
+            JsonParser.Event evt = parser.next();
+            switch (evt) {
+                case KEY_NAME:
+                    key = parser.getString();
+                    break;
+                case VALUE_STRING:
+                    val = parser.getString();
+                    mapKeyValue.put(key, val);
+                    break;
+                case VALUE_NUMBER:
+                    val = Integer.toString(parser.getInt());
+                    mapKeyValue.put(key, val);
+                    break;
+            }
+        }
+        System.out.println(mapKeyValue);
+        doPostOrPutOrDelete("INSERT INTO product (name, description, quantity) VALUES ( ?, ?, ?)",
+                mapKeyValue.get("name"), mapKeyValue.get("description"), mapKeyValue.get("quantity"));
     }
+
+    @PUT
+    @Path("{id}")
+    @Consumes("application/json")
+    public void doPut(@PathParam("id") String id, String str) {
+        JsonParser parser = Json.createParser(new StringReader(str));
+        Map<String, String> mapKayValue = new HashMap<>();
+        String key = "", val;
+        while (parser.hasNext()) {
+            JsonParser.Event evt = parser.next();
+            switch (evt) {
+                case KEY_NAME:
+                    key = parser.getString();
+                    break;
+                case VALUE_STRING:
+                    val = parser.getString();
+                    mapKayValue.put(key, val);
+                    break;
+                case VALUE_NUMBER:
+                    val = parser.getString();
+                    mapKayValue.put(key, val);
+                    break;
+            }
+        }
+        System.out.println(mapKayValue);
+        doPostOrPutOrDelete("UPDATE PRODUCT SET name = ?, description = ?, quantity = ? WHERE productID = ?",
+                mapKayValue.get("name"), mapKayValue.get("description"), mapKayValue.get("quantity"), id);
+
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void doDelete(@PathParam("id") String id, String str) {
+        doPostOrPutOrDelete("DELETE FROM product WHERE productID = ?", id);
+    }
+
+    private void doPostOrPutOrDelete(String query, String... params) {
+        try (Connection conn = getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        getResults("SELECT * FROM product");
+    }
+
+    
+
+    private String getResults(String query, String... params) {
+        String result = new String();
+        try (Connection conn = getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            ResultSet rs = pstmt.executeQuery();
+            JSONArray productArr = new JSONArray();
+            while (rs.next()) {
+                Map productMap = new LinkedHashMap();
+                productMap.put("productID", rs.getInt("productID"));
+                productMap.put("name", rs.getString("name"));
+                productMap.put("description", rs.getString("description"));
+                productMap.put("quantity", rs.getInt("quantity"));
+                productArr.add(productMap);
+            }
+            result = productArr.toString();
+        } catch (SQLException ex) {
+            Logger.getLogger(products.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result.replace("},", "},\n");
+    }
+    
 }
