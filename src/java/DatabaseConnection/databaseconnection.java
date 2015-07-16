8c4abc850b186c167619e4c0dd9eb6ca193089f5
c0654504 <c0654504@SB-N208-08.lambton.on.ca>
2015-07-16 17:26:15 -0400

    created databaseconnection class

---
+++ src/java/DatabaseConnection/databaseconnection.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DatabaseConnection;
+
+/**
+ *
+ * @author c0654504
+ */
+public class databaseconnection {
+    
+}
